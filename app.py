a444a6c2aa58bbe896754cf03887b031bda170a7
Dhruv Mandal <mandal.dhruv654@gmail.com>
2024-03-18 14:16:30 +0530

    First Commit

--- app.py
+++ app.py
@@ -7,23 +7,26 @@ obj=Flask(__name__)
 def welcome():
     return "Welcome to the Flask!"
 
-@obj.route('/cal', methods = ["GET"])
+@obj.route('/cal',methods=["GET"])
 def math_operator():
     operation=request.json["operation"]
     number1=request.json["number1"]
     number2=request.json["number2"]
-
-if operation=="add":
-    result=number1+number2
-elif operation=="multiply":
-    result=number1*number2
-elif operation=="division":
-    result=number1/number2   
-else: 
-    result=number1-number2   
+    
+    if operation=="add":
+        result=int(number1)+int(number2)
+    elif operation=="multiply":
+        result=int(number1)*int(number2)
+    elif operation=="division":
+        result=int(number1)/int(number2)
+    else:
+        result=int(number1)-int(number2)
+    return "the operation is {} and the result is {}".format(operation,result)
+        
+    
 
 
 print(__name__)
 
-if __name__== "__main__":
-    obj.run(debug=True)
+if __name__ == '__main__':
+    obj.run()
